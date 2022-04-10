e20cb9e2d0524be0f8f23152c2310182cb3f895e
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-04-10 19:04:15 +0530

    Code

--- src/main/java/com/example/springbootmongodb/controllers/ProductController.java
+++ src/main/java/com/example/springbootmongodb/controllers/ProductController.java
@@ -4,44 +4,46 @@ import com.example.springbootmongodb.commands.ProductForm;
 import com.example.springbootmongodb.converters.ProductToProductForm;
 import com.example.springbootmongodb.domain.Product;
 import com.example.springbootmongodb.services.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
  * @version 1.0
  * @since 10/04/22
  */
-@RestController
+@Controller
+@AllArgsConstructor
 public class ProductController {
-	private ProductService productService;
+	private final ProductService productService;
 
-	private ProductToProductForm productToProductForm;
+	private final ProductToProductForm productToProductForm;
 
 	@RequestMapping("/")
-	public String redirToList(){
+	public String redirToList() {
 		return "redirect:/product/list";
 	}
 
-	@RequestMapping({"/product/list", "/product"})
-	public String listProducts(Model model){
+	@RequestMapping({ "/product/list", "/product" })
+	public String listProducts(Model model) {
 		model.addAttribute("products", productService.listAll());
 		return "product/list";
 	}
 
 	@RequestMapping("/product/show/{id}")
-	public String getProduct(@PathVariable String id, Model model){
+	public String getProduct(@PathVariable String id, Model model) {
 		model.addAttribute("product", productService.getById(id));
 		return "product/show";
 	}
 
 	@RequestMapping("product/edit/{id}")
-	public String edit(@PathVariable String id, Model model){
+	public String edit(@PathVariable String id, Model model) {
 		Product product = productService.getById(id);
 		ProductForm productForm = productToProductForm.convert(product);
 
@@ -50,15 +52,15 @@ public class ProductController {
 	}
 
 	@RequestMapping("/product/new")
-	public String newProduct(Model model){
+	public String newProduct(Model model) {
 		model.addAttribute("productForm", new ProductForm());
 		return "product/productform";
 	}
 
 	@RequestMapping(value = "/product", method = RequestMethod.POST)
-	public String saveOrUpdateProduct(@Validated ProductForm productForm, BindingResult bindingResult){
+	public String saveOrUpdateProduct(@Validated ProductForm productForm, BindingResult bindingResult) {
 
-		if(bindingResult.hasErrors()){
+		if (bindingResult.hasErrors()) {
 			return "product/productform";
 		}
 
@@ -68,7 +70,7 @@ public class ProductController {
 	}
 
 	@RequestMapping("/product/delete/{id}")
-	public String delete(@PathVariable String id){
+	public String delete(@PathVariable String id) {
 		productService.delete(id);
 		return "redirect:/product/list";
 	}
